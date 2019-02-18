443642f7265be1e41ffe80e980ea0ad9f2dc3022
zhangzhibin94 <986846382@qq.com>
2019-02-18 10:20:48 +0800

    插入排序

--- src/test/java/TestArrayUtils.java
+++ src/test/java/TestArrayUtils.java
@@ -15,7 +15,11 @@ public class TestArrayUtils {
         List<Long> testIds2 = new ArrayList<>(15);
         testIds.add(123L);
         testIds.add(1234L);
+        testIds.stream().forEach(id->{
+            if(id.equals("123")){
 
+            }
+        });
         System.out.println(Arrays.toString(testIds.toArray()));
         String name = "1234567890";
         char[] chars = name.toCharArray();
